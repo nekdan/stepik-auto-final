9fa975e63e782b7e6798e8dfc6dcc1612fe9fcfe
nekdan <nekdan@ya.ru>
2021-09-25 16:42:38 +0300

    Добавил проверки для страница логина

---
+++ pages/login_page.py
@@ -0,0 +1,20 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        assert "login" in self.browser.current_url, "Некорректный URL адресс"
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Форма логина не найдена"
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Форма регистрации не найдена"
