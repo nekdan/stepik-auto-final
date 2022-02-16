61bf3be9cdcc83a8228ed3bf494a885ee2066bd1
danilushkin <nik.danilushkin@smartway.today>
2022-02-16 15:58:56 +0300

    4.3.4 Проверка на независимость контента. Параметризация

--- test_product_page.py
+++ test_product_page.py
@@ -6,10 +6,11 @@ from pages.product_page import ProductPage
 import math
 
 @pytest.mark.debug
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+@pytest.mark.parametrize('promo_offer', ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"])
+def test_guest_can_add_product_to_basket(browser, promo_offer):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_offer}"
     page = ProductPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()  # открываем страницу
     page.add_to_basket()
     page.solve_quiz_and_get_code()
-    time.sleep(10)
+    time.sleep(3)
