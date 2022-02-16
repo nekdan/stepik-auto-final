e077bbdb994d506337bfe83221918acbe4df36ba
danilushkin <nik.danilushkin@smartway.today>
2022-02-16 17:03:31 +0300

    4.3.4 Проверка на независимость контента. mark xfail

--- test_product_page.py
+++ test_product_page.py
@@ -6,7 +6,9 @@ from pages.product_page import ProductPage
 import math
 
 @pytest.mark.debug
-@pytest.mark.parametrize('promo_offer', ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"])
+@pytest.mark.parametrize('promo_offer', ["0", "1", "2", "3", "4", "5", "6",
+                                         pytest.param("7", marks=pytest.mark.xfail),
+                                         "8", "9"])
 def test_guest_can_add_product_to_basket(browser, promo_offer):
     link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_offer}"
     page = ProductPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
