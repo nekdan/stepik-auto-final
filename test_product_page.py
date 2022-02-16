183157ff6bdd5eb3e9301f4625cd87031e07d0d8
danilushkin <nik.danilushkin@smartway.today>
2022-02-16 15:03:09 +0300

    4.3.3 Проверка на независимость данных

--- test_product_page.py
+++ test_product_page.py
@@ -7,7 +7,7 @@ import math
 
 @pytest.mark.debug
 def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
     page = ProductPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()  # открываем страницу
     page.add_to_basket()
