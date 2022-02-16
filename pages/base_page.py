342c4671ebf678d1376f2757f9c5addde088d47a
danilushkin <nik.danilushkin@smartway.today>
2022-02-17 00:19:31 +0300

    4.3.8 наследование

--- pages/base_page.py
+++ pages/base_page.py
@@ -3,6 +3,7 @@ import math
 from selenium.common.exceptions import NoAlertPresentException
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from .locators import BasePageLocators
 
 class BasePage():
 
@@ -13,6 +14,13 @@ class BasePage():
 
     def open(self):
         self.browser.get(self.url)
+
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Ссылка на логин не найдена"
 
     def is_element_present(self, how, what):
         try:
